eb06da66fa954e16a1a8b284a9c8e63937852740
Mikhail Malakhov <malakhv@gmail.com>
2023-07-31 22:27:05 +0300

    Added Lazarus package project

---
+++ PascalKit.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit PascalKit;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  Mikhan.Util.StrUtils, Mikhan.Util.AppArgs, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('PascalKit', @Register);
+end.
