dd0f54037955d5cba7d6075c33c3cea86b114e1b
Mikhail Malakhov <malakhv@gmail.com>
2023-11-19 12:25:25 +0300

    Added code snippets

---
+++ snippets/Pair.pas
@@ -0,0 +1,11 @@
+BytesPair = packed record
+    First: Byte;
+    Second: Byte;
+    procedure Reverse();
+end;
+
+BytesTrio = packed record
+    First: Byte;
+    Second: Byte;
+    Third: Byte;
+end;
